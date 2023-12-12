14173b95d2b746a9ded462ab5ab414172fc91cd4
Aaron Lal <lucidlear@gmail.com>
2023-12-13 07:47:21 +1100

    Demo: variables and constants

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,23 +1,11 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace HelloWorld
+﻿namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            byte a = 1;
-
-            {
-                byte b = 2;
-                
-                {
-                    byte c = 3;
-                    Console.WriteLine(a);
-                    Console.Beep();
-                }
-            }
-            
+            const float Pi = 3.14f;
+            Pi = 1;
         }
     }
 }
