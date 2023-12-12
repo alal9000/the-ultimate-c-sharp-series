dd915d23a160390647ca9cd7d9e82e629f0c265d
Aaron Lal <lucidlear@gmail.com>
2023-12-12 20:16:56 +1100

    scope

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,15 +1,22 @@
-﻿namespace HelloWorld
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            byte a = 1;
 
-            byte number = 255;
-            number++;
-
-            Console.WriteLine(number);
-            Console.Beep();
+            {
+                byte b = 2;
+                
+                {
+                    byte c = 3;
+                    Console.WriteLine(a);
+                    Console.Beep();
+                }
+            }
             
         }
     }
