3035ee1f166a6be9b12aec353409caca59637625
Aaron Lal <lucidlear@gmail.com>
2023-12-14 08:30:54 +1100

    Demo: Strings

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,26 +1,29 @@
-﻿using HelloWorld.Math;
-
-namespace HelloWorld
+﻿namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var numbers = new int[3];
-            numbers[0] = 1;
+            var firstName = "Mosh";
+            var lastName = "Hamedani";
 
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[2]);
+            var fullName = firstName + " " + lastName;
 
-            var flags = new bool[3];
-            flags[0] = true;
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
 
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(", ", names);
 
-            var names = new string[3] { "Jack", "John", "Mary" };
+            
+
+            var text = @"Hi John
+Look into the following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+
+            Console.WriteLine(text);
+
+
         }
     }
 }
