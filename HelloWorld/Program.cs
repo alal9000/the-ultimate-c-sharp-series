6a05b2d84a37309cd98de73ee6ba5bec3b5d3784
Aaron Lal <lucidlear@gmail.com>
2023-12-16 20:15:09 +1100

    Demo: arrays

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,24 +4,46 @@
     {
         static void Main(string[] args)
         {
-            int total = 0;
-            while (true)
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+
+            // Length
+            Console.WriteLine("Length: " + numbers.Length);
+
+            // IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
+
+            // Clear()
+            Array.Clear(numbers, 0, 2);
+
+            Console.WriteLine("Effect of clear()");
+            foreach (var number in numbers)
             {
-                
-                Console.Write("enter a number: ");
-                var input = Console.ReadLine();
-
-                if (input == "ok") 
-                {
-                    break;
-                }
-
-                var result = Convert.ToInt32(input);
-                total += result;
-                Console.WriteLine(total);
-
+                Console.WriteLine(number);
             }
-            
+
+            // Copy()
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+
+            Console.WriteLine("Effect of copy");
+            foreach(var n in another)
+                Console.WriteLine(n);
+
+            // Sort()
+            Array.Sort(numbers);
+
+            Console.WriteLine("Effect of Sort()");
+            foreach( var n in numbers)
+                Console.WriteLine(n);
+
+            // Reverse()
+            Array.Reverse(numbers);
+
+            Console.WriteLine("Effect of Reverse()");
+            foreach(var number in numbers)
+                Console.WriteLine(number);
+
         }
     }
 }
