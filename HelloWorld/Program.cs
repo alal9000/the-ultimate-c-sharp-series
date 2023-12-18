808bd8010f91ad73f5d5afd004084b626630fdf0
Aaron Lal <lucidlear@gmail.com>
2023-12-18 15:50:34 +1100

    Control flow exercises part 2: 4

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,34 +6,26 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int>() { 1, 2, 3, 4 };
-            numbers.Add(1);
-            numbers.AddRange(new int[3] { 5, 6, 7 });
+            var secretNumber = new Random().Next(1, 11);
+            Console.WriteLine(secretNumber);
 
-            foreach (var number in numbers)
+            var input = "";
+            var convertedInput = 0;
+            int count = 0;
+            while (count < 4) 
             {
-                Console.WriteLine(number);
+                Console.Write("Pick a number: ");
+                input = Console.ReadLine();
+                convertedInput = Convert.ToInt32(input);
+                Console.WriteLine(convertedInput);
+
+                if (convertedInput == secretNumber)
+                    break;
+
+                count++;   
             }
-
-            Console.WriteLine();
-            Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
-            Console.WriteLine("Index of 1: " + numbers.LastIndexOf(1));
-
-            Console.WriteLine("Count: " + numbers.Count);
-
-            for (var i = 0; i < numbers.Count; i++)
-            {
-                if (numbers[i] == 1)
-                    numbers.Remove(numbers[i]);
-            }
-
-            foreach (var number in numbers)
-                Console.WriteLine(number);
-
-            numbers.Clear();
-            Console.WriteLine("Count: " + numbers.Count);
-
-
+            var result = (convertedInput != secretNumber) ? "lost" : "won";
+            Console.WriteLine(result);
         }
     }
 }
