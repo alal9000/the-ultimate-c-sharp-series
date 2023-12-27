7e69aa3d8ef1289d551d2ae2871776145c5ea9a7
Aaron Lal <lucidlear@gmail.com>
2023-12-28 10:55:21 +1100

    Control flow exercises part 1: 1 - added comments from mosh solution

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,11 +7,11 @@
             Console.WriteLine("Enter number between 1 and 10");
             var input = Console.ReadLine();
 
-            int convertedInput = Convert.ToInt32(input);
+            int convertedInput = Convert.ToInt32(input); // try to be consistent with variable conventions and change int to var here
 
-            if (convertedInput > 0 && convertedInput < 11)
+            if (convertedInput > 0 && convertedInput < 11) // by making the condition (number >= 1 && number <= 10) makes your code slightly more readable
             {
-                Console.WriteLine("Valid");
+                Console.WriteLine("Valid"); // we can remove curly braces to reduce an extra 4 lines of code as we only have one expression here 
             }     
             else
             {
