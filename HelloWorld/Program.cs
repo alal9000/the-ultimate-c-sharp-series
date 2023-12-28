8b341b01cf6f8f5a1d9c2f998c8d944dbb25c07e
Aaron Lal <lucidlear@gmail.com>
2023-12-28 12:47:45 +1100

    Control flow exercises part 1: 3 - mosh solution

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,14 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number: ");
-            var number1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter width of image: ");
+            //var width = Console.ReadLine();
 
-            Console.Write("Enter another number: ");
-            var number2 = Convert.ToInt32(Console.ReadLine());
+            //int convertedWidth = Convert.ToInt32(width);
 
-            var max = (number1 > number2) ? number1 : number2;
-            Console.WriteLine("Max is " + max);
+            //Console.Write("Enter height of image: ");
+            //var height = Console.ReadLine();
+
+            //int convertedHeight = Convert.ToInt32(height);
+
+            //if (convertedWidth > convertedHeight)
+            //{
+
+            //    Console.WriteLine("landscape");
+            //}
+            //else if (convertedHeight > convertedWidth)
+            //{
+            //    Console.WriteLine("Portrait");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("even");
+            //}
+
+            //mosh solution
+            Console.Write("Image width: ");
+            var width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Image height: ");
+            var height = Convert.ToInt32(Console.ReadLine());
+
+            var orientation = width > height ? ImageOrientation.Landscape : ImageOrientation.Portrait;
+            Console.WriteLine("Image orientation is " + orientation);
+
+                
+
+
+        }
+        public enum ImageOrientation
+        {
+            Landscape,
+            Portrait
         }
     }
 }
