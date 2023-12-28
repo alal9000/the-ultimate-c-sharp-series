0e09ee7336e38095eff5e448e87bccbc476cd642
Aaron Lal <lucidlear@gmail.com>
2023-12-28 11:23:43 +1100

    Control flow exercises part 1: 2 - mosh solution

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,21 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter number between 1 and 10");
-            var input = Console.ReadLine();
+            Console.WriteLine("Enter a number: ");
+            var number1 = Convert.ToInt32(Console.ReadLine());
 
-            int convertedInput = Convert.ToInt32(input); // try to be consistent with variable conventions and change int to var here
+            Console.Write("Enter another number: ");
+            var number2 = Convert.ToInt32(Console.ReadLine());
 
-            if (convertedInput > 0 && convertedInput < 11) // by making the condition (number >= 1 && number <= 10) makes your code slightly more readable
-            {
-                Console.WriteLine("Valid"); // we can remove curly braces to reduce an extra 4 lines of code as we only have one expression here 
-            }     
-            else
-            {
-                Console.WriteLine("Invalid");
-            }
-                
-
+            var max = (number1 > number2) ? number1 : number2;
+            Console.WriteLine("Max is " + max);
         }
     }
 }
