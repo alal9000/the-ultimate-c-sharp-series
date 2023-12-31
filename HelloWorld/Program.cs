3c477c1c970521d706e7a1770858e1faaaa06385
Aaron Lal <lucidlear@gmail.com>
2023-12-31 14:15:44 +1100

    Control flow exercises part 2: 3 - mosh solution

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,38 +4,28 @@
     {
         static void Main(string[] args)
         {
-            int total = 0;      // use var here
-            while (true)
+            Console.Write("Enter a number: ");
+            var input = Console.ReadLine();
+            var conversion = Convert.ToInt32(input);
+
+            int total = 1;
+            for (int i = conversion; i > 0; i--)
             {
-
-                Console.Write("enter a number: ");
-                var input = Console.ReadLine();
-
-                if (input == "ok")
-                {       // can remove left and right curly brace as only one expression for if block
-                    break;
-                }
-
-                var result = Convert.ToInt32(input); // this and bottom line can be converted to one and we dont need the extra variable result, we can re-use total ie: total += Convert.ToInt32(input);
-                total += result;
-                Console.WriteLine(total);
-
+                total = i * total;
             }
 
+            var result = String.Format("{0}! = {1}", input, total);
+            Console.WriteLine(result);
+
             // mosh solution
+            //Console.Write("Enter a number: ");
+            //var number = Convert.ToInt32(Console.ReadLine());
 
-            //var sum = 0;
-            //while (true)
-            //{
-            //    Console.Write("Enter a number (or 'ok' to exit): ");
-            //    var input = Console.ReadLine();
+            //var factorial = 1;
+            //for (var i = 1; i <= number; i++)
+            //    factorial *= i;
 
-            //    if (input.ToLower() == "ok")
-            //        break;
-
-            //    sum += Convert.ToInt32(input);
-            //}
-            //Console.WriteLine("Sum of all numbers is: " + sum);
+            //Console.WriteLine("{0}! = {1}", number, factorial);
 
         }
     }
