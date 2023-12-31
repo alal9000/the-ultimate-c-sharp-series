d08baca36d1e64fd16a3b8030f75bdfea961b3c3
Aaron Lal <lucidlear@gmail.com>
2023-12-31 13:46:18 +1100

    Control flow exercises part 2: 2 - mosh solution

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,20 +4,38 @@
     {
         static void Main(string[] args)
         {
-            int count = 0;
-            for (int i = 1; i <= 100; i++)
-                if (i % 3 == 0)
-                    count++;
-            Console.WriteLine(count);
+            int total = 0;      // use var here
+            while (true)
+            {
+
+                Console.Write("enter a number: ");
+                var input = Console.ReadLine();
+
+                if (input == "ok")
+                {       // can remove left and right curly brace as only one expression for if block
+                    break;
+                }
+
+                var result = Convert.ToInt32(input); // this and bottom line can be converted to one and we dont need the extra variable result, we can re-use total ie: total += Convert.ToInt32(input);
+                total += result;
+                Console.WriteLine(total);
+
+            }
 
             // mosh solution
-            //var count = 0;
-            //for (var i = 1; i <= 100; i++)
+
+            //var sum = 0;
+            //while (true)
             //{
-            //    if (i % 3 == 0)
-            //        count++;
+            //    Console.Write("Enter a number (or 'ok' to exit): ");
+            //    var input = Console.ReadLine();
+
+            //    if (input.ToLower() == "ok")
+            //        break;
+
+            //    sum += Convert.ToInt32(input);
             //}
-            //Console.WriteLine("There are {0} numbers divisible by 3 between 1 and 100.", count);
+            //Console.WriteLine("Sum of all numbers is: " + sum);
 
         }
     }
