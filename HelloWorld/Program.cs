4a7b0b7fee6540875b3eaf1bd7e10aed13d4ccec
Aaron Lal <lucidlear@gmail.com>
2024-01-07 20:16:38 +1100

    TimeSpan

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,22 +4,29 @@
     {
         static void Main(string[] args)
         {
-            var datetime = new DateTime(2015, 1, 1);
-            var now = DateTime.Now;
-            var today = DateTime.Today;
+            // Creating
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
 
-            //Console.WriteLine("Hour: " + now.Hour);
-            //Console.WriteLine("minute: " + now.Minute);
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
 
-            var tommorrow = now.AddDays(1);
-            var yesterday = now.AddDays(-1);
+            // Properties
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
 
-            Console.WriteLine(now.ToLongDateString());
-            Console.WriteLine(now.ToShortDateString());
-            Console.WriteLine(now.ToLongTimeString());
-            Console.WriteLine(now.ToShortTimeString());
-            Console.WriteLine(now.ToString("yyy-MM-dd HH:mm"));
+            // Add
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Subtract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
 
+            // ToString
+            Console.WriteLine("ToString" + timeSpan.ToString());
+
+            // Parse
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
         }
     }
 }
