9b764df192a5ba62cd4a76a2fdd610de37a13b96
Aaron Lal <lucidlear@gmail.com>
2024-01-09 15:01:53 +1100

    added mosh solutions to arrays and lists exercises

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -26,6 +26,29 @@
                     Console.WriteLine(namesList[0] + " and " + namesList[1] + " likes your post");
                 else
                     Console.WriteLine(namesList[0] + " and " + namesList[1] + " and " + (namesList.Count - 2) + " others likes your post");
+
+
+                // mosh solution
+                //var names = new List<string>();
+
+                //while (true)
+                //{
+                //    Console.Write("Type a name (or hit ENTER to quit): ");
+
+                //    var input = Console.ReadLine();
+                //    if (String.IsNullOrWhiteSpace(input))
+                //        break;
+                //    names.Add(input);
+                //}
+
+                //if (names.Count > 2)
+                //    Console.WriteLine("{0}, {1} and {2} others like your post", names[0], names[1], names.Count - 2);
+                //else if (names.Count == 2)
+                //    Console.WriteLine("{0} and {1} like your post", names[0], names[1]);
+                //else if (names.Count == 1)
+                //    Console.WriteLine("{0} likes your post.", names[0]);
+                //else
+                //    Console.WriteLine();
             }
 
             void ex2()
@@ -40,6 +63,17 @@
                 Array.Reverse(array);
                 var newString = new String(array);
                 Console.WriteLine(newString);
+
+                //mosh solution
+                //Console.Write("What's your name? ");
+                //var name = Console.ReadLine();
+
+                //var array = new char[name.Length];
+                //for (var i = name.Length; i > 0; i--)
+                //    array[name.Length - i] = name[i - 1];
+
+                //var reversed = new string(array);
+                //Console.WriteLine("Reversed name: " + reversed);
             }
 
             void ex3()
@@ -74,6 +108,27 @@
                 {
                     Console.WriteLine(item);
                 }
+
+                // mosh solution
+                //var numbers = new List<int>();
+
+                //while (numbers.Count < 5)
+                //{
+                //    Console.Write("Enter a number: ");
+                //    var number = Convert.ToInt32(Console.ReadLine());
+                //    if (numbers.Contains(number))
+                //    {
+                //        Console.WriteLine("You've previously entered " + number);
+                //        continue;
+                //    }
+
+                //    numbers.Add(number);
+                //}
+
+                //numbers.Sort();
+
+                //foreach (var number in numbers)
+                //    Console.WriteLine(number);
             }
 
             void ex4()
@@ -96,6 +151,31 @@
 
                 foreach (var item in set)
                     Console.WriteLine(item);
+
+                // mosh solution
+                //var numbers = new List<int>();
+
+                //while (true)
+                //{
+                //    Console.Write("Enter a number (or 'Quit' to exit): ");
+                //    var input = Console.ReadLine();
+
+                //    if (input.ToLower() == "quit")
+                //        break;
+
+                //    numbers.Add(Convert.ToInt32(input));
+                //}
+
+                //var uniques = new List<int>();
+                //foreach (var number in numbers)
+                //{
+                //    if (!uniques.Contains(number))
+                //        uniques.Add(number);
+                //}
+
+                //Console.WriteLine("Unique numbers:");
+                //foreach (var number in uniques)
+                //    Console.WriteLine(number);
 
             }
 
