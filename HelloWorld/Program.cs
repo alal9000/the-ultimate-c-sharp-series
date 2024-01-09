5efc45297b600323e1979f8bcd97e667e865485d
Aaron Lal <lucidlear@gmail.com>
2024-01-10 10:31:40 +1100

    final version of array and lists exercises with mosh solution

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -179,11 +179,78 @@
 
             }
 
-            ex4();
-            
+            void ex5()
+            {
 
 
-        
-        }
+                while (true)
+                {
+                    var numbers = new List<int>();
+
+                    Console.WriteLine("supply list of comma seperated numbers");
+                    var input = Console.ReadLine();
+                    var inputArray = input.Split(",");
+
+                    foreach (var item in inputArray)
+                    {
+                        numbers.Add(Convert.ToInt32(item));
+                    }
+
+                    if (numbers.Count < 5)
+                        Console.WriteLine("Invalid List, retry");
+                    continue;
+
+  
+                }
+
+
+                // mosh solution
+
+                //string[] elements;
+                //while (true)
+                //{
+                //    Console.Write("Enter a list of comma-separated numbers: ");
+                //    var input = Console.ReadLine();
+
+                //    if (!String.IsNullOrWhiteSpace(input))
+                //    {
+                //        elements = input.Split(',');
+                //        if (elements.Length >= 5)
+                //            break;
+                //    }
+
+                //    Console.WriteLine("Invalid List");
+                //}
+
+                //var numbers = new List<int>();
+                //foreach (var number in elements)
+                //    numbers.Add(Convert.ToInt32(number));
+
+                //var smallests = new List<int>();
+                //while (smallests.Count < 3)
+                //{
+                //    // Assume the first number is the smallest
+                //    var min = numbers[0];
+                //    foreach (var number in numbers)
+                //    {
+                //        if (number < min)
+                //            min = number;
+                //    }
+                //    smallests.Add(min);
+
+                //    numbers.Remove(min);
+                //}
+
+                //Console.WriteLine("The 3 smallest numbers are: ");
+                //foreach (var number in smallests)
+                //    Console.WriteLine(number);
+
+            }
+            ex5();
+
+
+
+
+            }
     }
 }
