561bc580428efddbb981bc45ed03b24aef656e28
Aaron Lal <lucidlear@gmail.com>
2024-01-11 10:01:58 +1100

    procedural programming example: 2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,20 +6,34 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.Write("Whats your name: ");
-            var name = Console.ReadLine();
-            var reversed = ReverseName(name);
-            Console.WriteLine("Reversed name: " + reversed);
+            var numbers = new List<int>();
+            while (true)
+            {
+                Console.Write("Enter a number or Quit to exit");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                    break;
+
+                numbers.Add(Convert.ToInt32(input));    
+            }
+
+            Console.WriteLine("Unique numbers:");
+            foreach (var number in GetUniqueNumbers(numbers))
+                Console.WriteLine(number);
 
         }
 
-        public static string ReverseName(string name)
+        public static List<int> GetUniqueNumbers(List<int> numbers)
         {
-            var array = new char[name.Length];
-            for (var i = name.Length; i > 0; i--)
-                array[name.Length - i] = name[i - 1];
+            var uniques = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+            }
 
-            return new string(array);
+            return uniques;
         }
 
         
