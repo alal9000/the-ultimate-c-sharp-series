5679d261e049335549ec6eda071ebfbfed0296bf
Aaron Lal <lucidlear@gmail.com>
2024-01-14 08:48:35 +1100

    methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,22 +1,51 @@
 ﻿namespace HelloWorld
 {
-    internal class Program
+    internal partial class Program
     {
-        public class Person
-        {
-            public int Id;
-            public string FirstName;
-            public string LastName;
-            public DateTime Birthdate;
-        }
 
         public static void Main(string[] args)
         {
-            var person = new Person
+            try
             {
-                FirstName = "Mosh",
-                LastName = "Hamedani"
-            };
+                var num = int.Parse("abc");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("conversion failed");
+            }    
+
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine(number);
+            else 
+                Console.WriteLine("conversion failed");
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3 }));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexcepected error occured");
+            }
         }
     }
 }
