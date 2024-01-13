c0ad63f4f044937ec29463d4c0df54b48860c5a8
Aaron Lal <lucidlear@gmail.com>
2024-01-13 19:26:20 +1100

    constructors

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,32 +1,18 @@
-﻿using System.Text;
-
-namespace HelloWorld
+﻿namespace HelloWorld
 {
-    public class Person
-    {
-        public string Name;
-
-        public void Introduce(string to)
-        {
-            Console.WriteLine("Hi {0}, I am {1}", to, Name);
-        }
-
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.Name = str;
-
-            return person;
-        }
-    }
 
     internal class Program
     {
 
         public static void Main(string[] args)
         {
-            var person = Person.Parse("John");
-            person.Introduce("Mosh");
+            var customer = new Customer(1, "john");
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
 
     }
