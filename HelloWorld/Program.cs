6284d341f5244d9bb3b360f9722d0f2dd45da837
Aaron Lal <lucidlear@gmail.com>
2024-01-15 17:56:26 +1100

    properties

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,27 +1,12 @@
 ﻿namespace HelloWorld
 {
-    public class Person
-    {
-        private DateTime _birthdate;
-
-        public void SetBirthdate(DateTime birthdate)
-        {
-            _birthdate = birthdate;
-        }
-
-        public DateTime GetBirthdate()
-        {
-            return _birthdate;
-        }
-    }
     internal partial class Program
     {
 
         public static void Main(string[] args)
         {
-            var person = new Person();
-            person.SetBirthdate(new DateTime(1982, 1, 1));
-            Console.WriteLine(person.GetBirthdate());
+            var person = new Person(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.Age);
         }
     }
 }
