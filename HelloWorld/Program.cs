addc6125242162cf45e30e5f4c7dfa000eb85864
Aaron Lal <lucidlear@gmail.com>
2024-01-15 14:51:48 +1100

    access modifiers

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,17 +1,27 @@
 ﻿namespace HelloWorld
 {
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
     internal partial class Program
     {
 
         public static void Main(string[] args)
         {
-            var customer = new Customer(1);
-            customer.Orders.Add(new Order());
-            customer.Orders.Add(new Order());
-
-            customer.Promote();
-
-            Console.WriteLine(customer.Orders.Count);
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
         }
     }
 }
