2157175a6865bdd83047b8cdbfdac244074990a6
Aaron Lal <lucidlear@gmail.com>
2024-01-16 16:01:27 +1100

    indexers

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,14 @@
 ﻿namespace HelloWorld
 {
+
     internal partial class Program
     {
-
+        
         public static void Main(string[] args)
         {
-            var person = new Person(new DateTime(1982, 1, 1));
-            Console.WriteLine(person.Age);
+            var cookie = new HttpCookie();
+            cookie["name"] = "mosh";
+            Console.WriteLine(cookie["name"]);
         }
     }
 }
