aa0f18ed993bd1cbad4e497fe712165609bed23b
Aaron Lal <lucidlear@gmail.com>
2024-01-18 13:27:39 +1100

    design a stopwatch exercise

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,14 +1,46 @@
-﻿namespace HelloWorld
+﻿using System.Text.RegularExpressions;
+
+namespace HelloWorld
 {
+    public class StopWatch
+    {
+        
+        private DateTime start;
+        private DateTime stop;
+        private TimeSpan _duration;
+
+        public TimeSpan Duration
+        {
+            get
+            {
+                // Calculate and return the duration based on the difference between start and stop
+                return stop - start;
+            }
+        }
+
+
+
+        public void Start()
+        {
+            start = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            stop = DateTime.Now;
+        }
+    }
 
     internal partial class Program
     {
         
         public static void Main(string[] args)
         {
-            var cookie = new HttpCookie();
-            cookie["name"] = "mosh";
-            Console.WriteLine(cookie["name"]);
+            StopWatch watch = new StopWatch();
+            watch.Start();
+            TimeSpan delay = TimeSpan.FromSeconds(10);
+            watch.Stop();
+            Console.WriteLine(watch.Duration + delay);
         }
     }
 }
