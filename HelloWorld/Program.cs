aa79ec21d98778e2f78f94ed17bc358fca61ab93
Aaron Lal <lucidlear@gmail.com>
2024-01-19 10:11:20 +1100

    inheritance

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,45 +2,15 @@
 
 namespace HelloWorld
 {
-    public class StopWatch
-    {
-        
-        private DateTime start;
-        private DateTime stop;
-        private TimeSpan _duration;
-
-        public TimeSpan Duration
-        {
-            get
-            {
-                // Calculate and return the duration based on the difference between start and stop
-                return stop - start;
-            }
-        }
-
-
-
-        public void Start()
-        {
-            start = DateTime.Now;
-        }
-
-        public void Stop()
-        {
-            stop = DateTime.Now;
-        }
-    }
 
     internal partial class Program
     {
         
         public static void Main(string[] args)
         {
-            StopWatch watch = new StopWatch();
-            watch.Start();
-            TimeSpan delay = TimeSpan.FromSeconds(10);
-            watch.Stop();
-            Console.WriteLine(watch.Duration + delay);
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
         }
     }
 }
